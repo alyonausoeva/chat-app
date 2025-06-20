78130a368e7f62f0402a64c3d9054714645b08d7
Alyona Usoeva <alyonausoeva@gmail.com>
2025-06-20 08:16:35 +0200

    Update api.ts

--- client/src/shared/constants/api.ts
+++ client/src/shared/constants/api.ts
@@ -1,2 +1,2 @@
 export const baseUrl = import.meta.env.VITE_API_URL || 'http://localhost:5050/jsonrpc';
-export const socketUrl = import.meta.env.SOCKET_API_URL || 'http://localhost:3000';
+export const socketUrl = import.meta.env.VITE_SOCKET_API_URL || 'http://localhost:3000';
